37408c019421ca7247b5d2ce28d827120f42a061
carl0bg <dmitrijkarlysev0493@gmail.com>
2024-04-04 12:34:05 +0300

    first commit

---
+++ store/products/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+from products.models import Product, ProductCategory, Basket
+from users.models import User
+
+
+def index(request):
+    # return HttpResponse('Hello bith')
+    context = {
+        'title':'Bookstore',
+    }
+    return render(request, 'products/index2.html', context=context)
+
+
+def products(request, category_id=None, page_number=1): #может и не быть category id, поэтому None
+    # if category_id:
+    #     # category = ProductCategory.objects.get(id = category_id)
+    #     products = Product.objects.filter(category_id = category_id)
+    # else:
+    #     products = Product.objects.all()
+
+    products = Product.objects.filter(category_id = category_id) if category_id else Product.objects.all()
+   
+    per_page = 3 #это сколько товаров продут показано
+    paginator = Paginator(products, per_page)
+    products_paginator = paginator.page(page_number)
+    
+    context = {
+        'title':'Store - Каталог',
+        'products': products_paginator,
+        'categories': ProductCategory.objects.all(),
+    }    
+    return render(request, 'products/products.html', context)
+
+
+@login_required
+def basket_add(request, product_id):
+    product = Product.objects.get(id = product_id)
+    baskets = Basket.objects.filter(user = request.user, product = product)
+    
+    if not baskets.exists():
+        Basket.objects.create(user = request.user, product = product, quantity = 1)
+    else:
+        basket = baskets.first()
+        basket.quantity+=1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER']) #возвращает на ту же страничку где было выполнено дейсствие 
+
+@login_required
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id = basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
---
+++ store/users/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth, messages
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from users.models import User
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from products.models import Basket
+
+# Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data = request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username = username, password = password) #сравним есть ли такой пользователь
+            if user:
+                auth.login(request, user) #если пользователь есть, мы его авторизовали
+                return HttpResponseRedirect(reverse('index')) #перенаправляем на главную страницу
+    else:
+        form = UserLoginForm()  
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data = request.POST)
+        if form.is_valid():
+            form.save() #сохраняем даныне в базе данных
+            messages.success(request, 'Поздравляем! Вы успешно зарегестрировались!')
+            return HttpResponseRedirect(reverse('users:login'))
+    else: 
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
+
+
+# def profile(request):
+#      if request.method == 'POST':
+#          form=UserProfileForm(instance = request.user, data = request.POST, files = request.FILES)
+#          if form.is_valid():
+#              form.save()
+#              return HttpResponseRedirect(reverse('users:profile'))
+#          else:
+#              print(form.errors)
+#      else:
+#         form = UserProfileForm(instance=request.user)
+#      baskets = Basket.objects.filter(user = request.user)
+#      total_sum = 0
+#      total_quintity = 0
+
+#      for bask in baskets:
+#          total_sum = total_sum*bask.sum()
+#          total_quintity = total_quintity+bask.quintity
+
+#      context={'title':'Store - Профиль', 'form': form, 'baskets': baskets, 'total_sum': total_sum, 'total_quintity': total_quintity}
+#      return render(request, 'users/profile.html', context)
+
+@login_required
+def profile(request):
+   if request.method == 'POST': #меняем имя, если захотим редактировать
+      form = UserProfileForm(instance = request.user, data = request.POST, files = request.FILES) #если заполним новые данные
+      if form.is_valid():                                                  #это чтобы прикрепить фотографию
+         form.save()
+         return HttpResponseRedirect(reverse('users:profile'))
+      else:
+         print(form.errors) #выводим в консоль ошибки
+   else:
+      form = UserProfileForm(instance = request.user) #сделано чтобы отображать данные в полях 
+
+   context = { 'title': 'Store-Профиль',
+               'form': form, 
+               'baskets': Basket.objects.filter(user = request.user),
+               # 'total_sum': total_sum, 
+               # 'total_quantity': total_quantity
+   } #нужно делать с фильтром, потому что будут видны все товары, добавленные даже другими пользоватлеями
+   #print(Product.objects.all().order_by('name'))
+   return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request) #пользователь выйдет из системы
+    return HttpResponseRedirect(reverse('index'))
+
