658d702573b6a0ab36ac39084b38796bace72ea3
carl0bg <dmitrijkarlysev0493@gmail.com>
2024-05-02 22:39:03 +0300

    create UserSerializer, APIView изменили encode, и проверка в post

--- store/users/serializers.py
+++ store/users/serializers.py
@@ -1,9 +1,45 @@
 
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 from users.models import User
+import io
+from rest_framework.parsers import JSONParser
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'first_name')
-        
+
+# class UserModel:
+#     def __init__(self, username, image):
+#         self.username = username
+#         self.image = image
+
+# class UserSerializer(serializers.Serializer):
+#     username = serializers.CharField(max_length = 255)
+#     image = serializers.CharField()
+
+
+class UserSerializer(serializers.Serializer):
+   first_name = serializers.CharField(max_length = 255, read_only = True)
+   last_name = serializers.CharField(read_only = True)
+   image = serializers.ImageField(read_only = True)
+   username = serializers.CharField()
+   email = serializers.CharField(read_only = True)
+   
+
+
+
+
+
+# def encode():
+#     model = UserModel('Dmitrii', 'Karl')
+#     model_sr = UserSerializer(model) #словарь
+#     print(model_sr.data, type(model_sr.data), sep = '\n') #сериализованный данные
+#     json = JSONRenderer().render(model_sr.data) #преобразует объект сериализации в байтовую json среду
+#     print(json)        
+
+
+# def decode():
+#     stream = io.BytesIO(b'{"username":"Dmitrii","image":"Karl"}')
+#     data = JSONParser().parse(stream)
+#     serializers = UserSerializer(data = data) # чтобы получить объект сериализации
+#     serializers.is_valid()
+#     print(serializers.validated_data) #результат декодирования json строки
+    
--- store/users/views.py
+++ store/users/views.py
@@ -11,6 +11,16 @@ from users.models import User #, EmailVerification
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
+#for email
+from django.core.mail import send_mail
+import string
+import secrets
+import uuid
+from store import settings
+from django.core.cache import cache
+import gettext
+_ = gettext.gettext
+
 
 from rest_framework import generics
 from rest_framework.response import Response
@@ -21,30 +31,28 @@ from .serializers import UserSerializer
 
 class APIView(APIView):
     def get(self, request):
-        lst = User.objects.all().values()
-        return Response({'stat': list(lst)})
+        # lst = User.objects.all().values()
+        # return Response({'stat': list(lst)})
+        u = User.objects.all() #формируется список обьектов
+        return Response({'posts': UserSerializer(u, many= True).data}) #many-список записей обрабатывать #по факту как наш encode
+
     
     def post(self, request):
-        post_new = User.objects.create(title = request.data['title'], context = request.data['content'], cat_id = request.data['cat_id'])
-        return Response({'post':model_to_dict(post_new)}) #преобразовывает обьект джанго в словарь
+        serializer = UserSerializer(data= request.data)
+        serializer.is_valid(raise_exception=True) #проверяем по UserSerial валидатность
+
+
+        post_new = User.objects.create(
+            username = request.data['username'],
+            password = request.data['password']
+        )
+        return Response({'post': UserSerializer(post_new).data}) #model_to_dict - преобразует в словарь
     
 
 # class APIView(generics.ListAPIView):
 #     queryset = User.objects.all()
 #     serializer_class = UserSerializer
 
-
-
-
-from django.core.mail import send_mail
-import string
-import secrets
-import uuid
-from store import settings
-from django.core.cache import cache
-import gettext
-
-_ = gettext.gettext
 
 
 
@@ -62,69 +70,6 @@ def login(request):
         form = UserLoginForm()  
     context = {'form': form}
     return render(request, 'users/login.html', context)
-
-
-# def registration(request):
-#     if request.method == 'POST':
-#         form = UserRegistrationForm(data = request.POST)
-#         if form.is_valid():
-#             form.save() #сохраняем даныне в базе данных
-#             messages.success(request, 'Поздравляем! Вы успешно зарегестрировались!')
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else: 
-#         form = UserRegistrationForm()
-#     context = {'form': form}
-#     return render(request, 'users/registration.html', context)
-
-
-# class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
-#     model = User
-#     form_class = UserRegistrationForm
-#     template_name = 'users/registration.html'
-#     success_url = reverse_lazy('users:login')
-#     success_message = 'Вы успешно зарегестрированы!'
-#     title = 'Store - Регистрация'
-
-
-# class UserRegistrationView(FormView):
-#     form_class = UserRegistrationForm
-#     template_name = 'users/registration.html'
-#     success_url = reverse_lazy('users:login')
-
-
-#     def form_valid(self, form):
-#         user, created = User.objects.get_or_create(username = form.cleaned_data['username'])
-#         print(user.email, user.username)
-#         new_pass = None
-
-#         if created:
-#             alphabet = string.ascii_letters + string.digits
-#             new_pass = ''.join(secrets.choice(alphabet) for i in range(8))
-#             user.set_password(new_pass)
-#             user.save(update_fields=["password", ])
-
-#         if new_pass or user.is_verified_email is False:
-#             token = uuid.uuid4().hex
-#             redis_key = settings.C_USER_CONFIRMATION_KEY.format(token=token)
-#             cache.set(redis_key, {"user_id": user.id}, timeout=settings.C_USER_CONFIRMATION_TIMEOUT)
-
-#             confirm_link = self.request.build_absolute_uri(
-#                 reverse_lazy(
-#                     "users:register_confirm", kwargs={"token": token}
-#                 )
-#             )
-#             message = _(f"follow this link %s \n"
-#                         f"to confirm! \n" % confirm_link)
-#             if new_pass:
-#                 message += f"Your new password {new_pass} \n "
-
-#             send_mail(
-#                 subject= "Please confirm your registration!",
-#                 message=message,
-#                 from_email="carlbg000@yandex.ru",
-#                 recipient_list=[user.email, ]
-#             )
-#         return super().form_valid(form)
 
 
 def form_email(request, form):
@@ -196,28 +141,7 @@ def register_confirm(request, token):
         return redirect(to=reverse_lazy("users:registration"))
 
 
-'''
-@login_required
-def profile(request):
-   if request.method == 'POST': #меняем имя, если захотим редактировать
-      form = UserProfileForm(instance = request.user, data = request.POST, files = request.FILES) #если заполним новые данные
-      if form.is_valid():                                                  #это чтобы прикрепить фотографию
-         form.save()
-         return HttpResponseRedirect(reverse('users:profile'))
-      else:
-         print(form.errors) #выводим в консоль ошибки
-   else:
-      form = UserProfileForm(instance = request.user) #сделано чтобы отображать данные в полях 
 
-   context = { 'title': 'Store-Профиль',
-               'form': form, 
-               'baskets': Basket.objects.filter(user = request.user),
-               # 'total_sum': total_sum, 
-               # 'total_quantity': total_quantity
-   } #нужно делать с фильтром, потому что будут видны все товары, добавленные даже другими пользоватлеями
-   #print(Product.objects.all().order_by('name'))
-   return render(request, 'users/profile.html', context)
-'''
 
 class profile(View):
     template_name = 'users/profile.html'
@@ -244,18 +168,4 @@ def logout(request):
     return HttpResponseRedirect(reverse('index'))
 
 
-# class EmailVerificationView(TitleMixin, TemplateView):
-#     title = 'Store - Подтверждение электронной почты'
-#     template_name = 'users/email_verification.html'
-
-#     def get(self, request, *args, **kwargs):
-#         code = kwargs['code']
-#         user = User.objects.get(email=kwargs['email'])
-#         email_verifications = EmailVerification.objects.filter(user = user, code = code)
-#         if email_verifications.exists() and not email_verifications.first().is_expired():
-#             user.is_verified_email = True
-#             user.save()
-#             return super(EmailVerificationView, self).get(request, *args, **kwargs)
-#         else:
-#             return HttpResponseRedirect(reverse('index'))
 
