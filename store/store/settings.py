28e40a7b8b1db4c133e9bd815bc908500775d744
carl0bg <dmitrijkarlysev0493@gmail.com>
2024-05-07 22:34:28 +0300

    start environ

--- store/store/settings.py
+++ store/store/settings.py
@@ -11,21 +11,56 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env= environ.Env(
+    DEBUG=(bool),
+    SECRET_KEY=(str),
+    DOMAIN_NAME=(str),
+
+    REDIS_HOST=(str),
+    REDIS_PORT=(str),
+
+    DATABASE_NAME=(str),
+    DATABASE_USER=(str),
+    DATABASE_PASSWORD=(str),
+    DATABASE_HOST=(str),
+    DATABASE_PORT=(str),
+
+
+    EMAIL_HOST=(str),
+    EMAIL_PORT=(int),
+    EMAIL_HOST_USER=(str),
+    EMAIL_HOST_PASSWORD=(str),
+    EMAIL_USE_SSL=(bool),
+    EMAIL_USE_TLS=(bool),
+    DEFAULT_FROM_EMAIL = (str),
+
+)
+# env = environ.Env()
+# environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / '.env')
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-in-sycu04+pu&s+bswslcp%$^sm!sk9c0u&cnm^grsb55h_g3x'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env('SECRET_KEY')
+
+
+DEBUG = env('DEBUG')
+
+
 
 ALLOWED_HOSTS = ['*']
+
+DOMAIN_NAME = env('DOMAIN_NAME')
 
 
 # Application definition
@@ -86,18 +121,19 @@ WSGI_APPLICATION = 'store.wsgi.application'
 #     }
 # }
 
+
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'store',
-    'USER': 'dima',
-    'PASSWORD': '2003',
-    'HOST': 'localhost',
-    'PORT': '5432',
-}
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+    }
 }
 
-DOMAIN_NAME = 'http://127.0.0.1:8000'  #не уверен что здесь нужно писать
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -147,32 +183,39 @@ MEDIA_ROOT= BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+
+
+#email
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = env('EMAIL_USE_SSL')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+
+
 #Users
 
 AUTH_USER_MODEL = 'users.User'
 
 LOGIN_URL = '/users/login/'
 
-###email###
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
 
+REDIS_HOST = env('REDIS_HOST')
+REDIS_PORT = env('REDIS_PORT')
 
-EMAIL_HOST_USER = 'carlbg000@yandex.ru'
-EMAIL_HOST_PASSWORD= 'lyzyczsewmlxwcdc'
-DEFAULT_FROM_EMAIL = 'carlbg000@yandex.ru'
-
-
+# Caches
 
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': f'redis://{REDIS_HOST}:{REDIS_PORT}/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
-
 C_USER_CONFIRMATION_KEY = "user_confirmation_{token}" #для редиса
 C_USER_CONFIRMATION_TIMEOUT = 300
 
